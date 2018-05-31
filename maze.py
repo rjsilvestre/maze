d0c2b36989d85cbd6e4ae14e57236bd1e966c27d
Rafael José <rjsilvestre@gmail.com>
2018-05-31 23:02:48 +0800

    Renamed path variable to paths when creating the graph

--- maze.py
+++ maze.py
@@ -17,15 +17,15 @@ class Maze:
             self._walls.add(tile)
 
     def make_graph(self):
-        path = [(x, y) for x in range(self._num_nodes)
+        paths = [(x, y) for x in range(self._num_nodes)
                 for y in range(self._num_nodes) if (x, y) not in self._walls]
-        self._graph = {(x, y): [] for x, y in path}
-        for x, y in path:
-            if (x+1, y) in path and (x+1, y) in self._graph:
+        self._graph = {(x, y): [] for x, y in paths}
+        for x, y in paths:
+            if (x+1, y) in paths and (x+1, y) in self._graph:
                 self._graph[(x, y)].append((x+1, y))
-            if (x-1, y) in path and (x-1, y) in self._graph:
+            if (x-1, y) in paths and (x-1, y) in self._graph:
                 self._graph[(x, y)].append((x-1, y))
-            if (x, y+1) in path and (x, y+1) in self._graph:
+            if (x, y+1) in paths and (x, y+1) in self._graph:
                 self._graph[(x, y)].append((x, y+1))
-            if (x, y-1) in path and (x, y-1) in self._graph:
+            if (x, y-1) in paths and (x, y-1) in self._graph:
                 self._graph[(x, y)].append((x, y-1))
